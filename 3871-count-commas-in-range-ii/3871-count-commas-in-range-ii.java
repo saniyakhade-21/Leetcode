cae17da7e90ad7f2d666c738d93c05a688a043d1
saniyakhade-21 <saniyakhade21@gmail.com>
2026-09-09 18:32:50 +0530

    Time: 1 ms (99.36%), Space: 42.6 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long start = 1000;
+        long commas = 1;
+
+        while (start <= n) {
+            long end = start * 1000 - 1;
+
+            if (end < start) { // overflow protection
+                end = n;
+            }
+
+            long count = Math.min(n, end) - start + 1;
+            ans += count * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return ans;
+    }
+}
